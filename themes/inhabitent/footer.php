da7bebe2966a86c0efacc5c9a9b0d268aedd13bb
Cedric Reichmuths <cedric.reichmuth@gamil.com>
2018-05-22 10:38:46 -0400

    work on styling, added functionality plugin

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,7 +11,36 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="footer-top">
+						<div class="contact">
+							<h3>Contact Info</h3>
+							<p class="mail">
+								<i class="fa fa-envelope"></i>
+								<a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+							</p>
+							<p>
+								<i class="fa fa-phone"></i>
+								<a href="tel:5553434567891">778-456-7891</a>
+							</p>
+							<p>
+								<span><i class="fab fa-facebook-square"></i></span>
+								<span><i class="fab fa-twitter-square"></i></span>
+								<span><i class="fab fa-google-plus-square"></i></span>
+							</p>
+						</div>
+						<div class="hours">
+							<h3>Business Hours</h3>
+							<p><span class="day-of-week"><b>Monday-Friday:</b></span> 9am to 5pm</p>
+							<p><span class="day-of-week"><b>Saturday:</b></span> 10am to 2pm</p>
+							<p><span class="day-of-week"><b>Sunday:</b></span> Closed</p>
+						</div>
+						<div class="footer-logo-container">
+							<a href="<?php get_home_url();?>">
+								<img src="<?php get_template_directory_uri();?>/images/logos/inhabitent-logo-text.svg" alt="inhabitent logo">
+							</a>
+						</div>
+					</div>
+					<p class="footer-bottom">COPYRIGHT © 2017 INHABITENT</p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="about-content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -19,5 +19,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
